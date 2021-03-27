f5d57d399e4d46d09f0f4e42f914d5cce5a9dbb9
Bibek_16 <71329649+Bibek117@users.noreply.github.com>
2021-03-27 20:59:36 +0545

    image gallery completed

--- mdnproject3imagegallery/main.js
+++ mdnproject3imagegallery/main.js
@@ -5,9 +5,29 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
+var imageGallery = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
+imageGallery.forEach((value)=>{
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
+newImage.setAttribute('src', `images/${value}`);
 thumbBar.appendChild(newImage);
+});
+thumbBar.addEventListener('click',(e)=>{
+    if(e.target && e.target.nodeName == "IMG"){
+        //console.log(e.target.src);
+        displayedImage.src = e.target.src;
+    }
+})
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click',()=>{
+    var choice = btn.getAttribute('class');
+    if(choice== "dark"){
+        btn.setAttribute('class','light');
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    }else{
+        btn.setAttribute('class','dark');
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+});
